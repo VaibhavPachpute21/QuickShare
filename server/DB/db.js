6df6ddcb01d3fd7d6b1563f91c1a1036d59d0612
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-05-01 20:51:50 +0530

    started download

--- server/DB/db.js
+++ server/DB/db.js
@@ -6,7 +6,7 @@ const ConnectDB=async ()=>{
         await mongoose.connect(MONGODB_URL,{useNewUrlParser:true});
         console.log("Database connected successfully...")
     } catch (error) {
-        console.error('Error while connecting database...')
+        console.error('Error while connecting database...',error)
     }
 }
 
--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,16 @@
 import multer from 'multer'
+import path from 'path'
 
-const upload=multer({dest:'uploads'})
+// const upload=multer({dest:'uploads',
+// })
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/') ,
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+              cb(null, uniqueName)
+    } ,
+});
+
+const upload =multer({ storage, limits:{ fileSize: 1000000 * 100 }, });
 
 export default upload;
