abdfe409aa52da69a6a38771b22cfbfa1ba90065
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-05-20 13:34:27 +0530

    download page

--- client/src/components/DownloadFilePage.js
+++ client/src/components/DownloadFilePage.js
@@ -2,11 +2,27 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom';
 
 export const DownloadFilePage = (props) => {
-    const {id}=useParams()
+  const { id } = useParams()
+
+  
+
+
   return (
-    <div className='container'>
-        <h2>Download File</h2>
-        <a href={`http://127.0.0.1:8080/file/${id}`} target='_blank' className='btn'>Download File</a>
+    <div className='container outercontainer '>
+      <div className='justify-content-center innerContainer shadow'>
+        <h2 className='text-center'>Download File</h2>
+        <div className='dropbox form-control form-control-lg'>
+          <a href={`http://127.0.0.1:8080/file/${id}`} target='_blank' className='btn'>Download File <i class="fa fa-download" ></i></a>
+          <div className='container'>
+            <div className='justify-content-start'>
+              <p>Name: Capture.png</p>
+              <p>Size: 9343 Mb</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
